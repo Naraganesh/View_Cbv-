22e50fd79742eb9f5c14823488e9c48fe95e4045
Naraganesh <naraganesh143@gmail.com>
2024-02-07 20:08:09 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse  
+from django.views.generic import View,TemplateView
+from app.models import * 
+from app.forms import *
+
+# Returning string as response by using FBV 
+def fbv_string(request):
+    return HttpResponse('<h1> This is the string from fbv_string ') 
+
+
+# Returning string as response by using class Based View 
+class CbvString(View):
+    def get(self,request):
+        return HttpResponse('string of CbvString ') 
+    
+# renduring html by FBV 
+    
+def fbvhtml(request):
+    return render(request,'fbvhtml.html') 
+
+
+
+# renduring html by CBV  
+class Cbv_Html(View):
+    def get(self,request):
+        return render(request, 'Cbv_Html.html') 
+    
+# insert data by FBV through Model Forms
+    
+def insert_school_by_fbv(request):
+    SFO=SchoolForm()
+    d={'SFO':SFO}
+
+    if request.method=='POST':
+        SFDO=SchoolForm(request.POST)
+        if SFDO.is_valid():
+            SFDO.save()
+            return HttpResponse('insert_school_by_fbv is done')
+    return render(request,'insert_school_by_fbv.html',d) 
+
+
+
+
+
+#Insert Data By using Class Based View 
+
+class InsertSchoolByCbv(View):
+    def get(self,request):
+        ESFO=SchoolForm()
+        d={'ESFO':ESFO}
+        return render(request,'InsertSchoolByCbv.html',d)
+    
+    def post(self,request):
+        SFDO=SchoolForm(request.POST)
+        if SFDO.is_valid():
+            SFDO.save()
+            return HttpResponse('InsertSchoolByCbv is done') 
+        
+
+    
+class Cbv_Template(TemplateView):
+    template_name='Cbv_Template.html'
+
+
+
+
+
+    
